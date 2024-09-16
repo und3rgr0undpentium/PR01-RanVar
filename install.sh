831cc1f85faa4b5044fd5fcf5c11ec08a39c8ae3
lakey <36199897+und3rgr0undpentium@users.noreply.github.com>
2024-09-16 14:48:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,19 @@ cd ..
 mv PR01-RanVar/* ~/.PR01-RanVar
 rm -rf PR01-RanVar
 cd ~/.PR01-RanVar
-echo [+] Completed
+echo [+_+_+_+_+_] Completed
 
 #  get tools------------------------------------------
 echo [*] Installing tools...
 sudo apt update
 sudo apt-get install python3
-echo [+] Completed
+echo [+_+_+_+_+_] Completed
 
 # set up alias workflow--------------------------------
 echo [*] Setting up alias...
 echo "alias ranvar=\"python3 $(pwd)/randomvar.py\"" >> ~/.bashrc
 echo "alias ranvar=\"python3 $(pwd)/randomvar.py\"" >> ~/.zshrc
-echo [+] Completed
+echo [+_+_+_+_+_] Completed
 
 # clean up---------------------------------------------
 echo [+] Installation Completed...
