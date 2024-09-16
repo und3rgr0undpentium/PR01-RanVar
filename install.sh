e2a5042b3b5f5a66998f8d7eb64d4e3374e8c477
lakey <36199897+und3rgr0undpentium@users.noreply.github.com>
2024-09-16 12:30:38 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# installer for RANDOMCHARVARIABLE
+# created by : lakey
+
+# staging--------------------------------------------
+echo [*] Staging process...
+mkdir ~/.PR01-RanVar
+cd ..
+mv PR01-RanVar/* ~/.PR01-RanVar
+rm -rf PR01-RanVar
+cd ~/.PR01-RanVar
+echo [+] Completed
+
+#  get tools------------------------------------------
+echo [*] Installing tools...
+sudo apt update
+sudo apt-get install python3
+echo [+] Completed
+
+# set up alias workflow--------------------------------
+echo [*] Setting up alias...
+echo "alias ranvar=\"python3 $(pwd)/main.py\"" >> ~/.bashrc
+echo "alias ranvar=\"python3 $(pwd)/main.py\"" >> ~/.zshrc
+echo [+] Completed
+
+# clean up---------------------------------------------
+echo [+] Installation Completed...
+echo "- please restart your terminal"
+echo "- type 'ranvar' to launch RanVar"
